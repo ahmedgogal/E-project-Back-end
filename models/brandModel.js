723b8ba032c48a9aa989a4b2481f4a29b9975d7e
ahmedgogal <ahmed_31070293@ics.tanta.edu.eg>
2024-02-09 11:44:09 +0200

    update postman api

--- models/brandModel.js
+++ models/brandModel.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // 1- Create Schema
 const brandSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'Brand required'],
-            unique: [true, 'Brand must be unique'],
-            minlength: [3, 'Too short Brand name'],
-            maxlength: [32, 'Too long Brand name'],
-        },
-        slug: {
-            type: String,
-            lowercase: true,
-        },
-        image: String,
+  {
+    name: {
+      type: String,
+      required: [true, "Brand required"],
+      unique: [true, "Brand must be unique"],
+      minlength: [3, "Too short Brand name"],
+      maxlength: [32, "Too long Brand name"],
     },
-    { timestamps: true }
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    image: String,
+  },
+  { timestamps: true }
 );
 
 // 2- Create model
-module.exports = mongoose.model('Brand', brandSchema);
+module.exports = mongoose.model("Brand", brandSchema);
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,32 +1,32 @@
-const express = require('express');
+const express = require("express");
 const {
-    getCategoryValidator,
-    createCategoryValidator,
-    updateCategoryValidator,
-    deleteCategoryValidator,
-} = require('../utils/validators/categoryValidator');
+  getCategoryValidator,
+  createCategoryValidator,
+  updateCategoryValidator,
+  deleteCategoryValidator,
+} = require("../utils/validators/categoryValidator");
 
 const {
-    getCategories,
-    getCategory,
-    createCategory,
-    updateCategory,
-    deleteCategory,
-} = require('../services/categoryService');
-const subcategoriesRoute = require('./subCategoryRoute');
+  getCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../services/categoryService");
+const subcategoriesRoute = require("./subCategoryRoute");
 
 const router = express.Router();
 
-router.use('/:categoryId/subcategories', subcategoriesRoute);
+router.use("/:categoryId/subcategories", subcategoriesRoute);
 
 router
-    .route('/')
-    .get(getCategories)
-    .post(createCategoryValidator, createCategory);
+  .route("/")
+  .get(getCategories)
+  .post(createCategoryValidator, createCategory);
 router
-    .route('/:id')
-    .get(getCategoryValidator, getCategory)
-    .put(updateCategoryValidator, updateCategory)
-    .delete(deleteCategoryValidator, deleteCategory);
+  .route("/:id")
+  .get(getCategoryValidator, getCategory)
+  .put(updateCategoryValidator, updateCategory)
+  .delete(deleteCategoryValidator, deleteCategory);
 
 module.exports = router;
