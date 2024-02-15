ef89165cb4ec39c38bcea368270020d8763aad48
ahmedgogal <ahmed_31070293@ics.tanta.edu.eg>
2024-02-16 01:06:45 +0200

    Done(13)

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -16,7 +16,7 @@ const sendEmail = async (options) => {
 
   // 2) Define email options (like from, to, subject, email content)
   const mailOpts = {
-    from: 'E-shop App <progahmedelsayed@gmail.com>',
+    from: 'ClickBuy <progahmedelsayed@gmail.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
